4a2a2e5bf434c977eafff764da3c7e68ddf8d1ca
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2020-10-16 23:26:47 +0530

    forum

--- learningApp/src/app/app.module.ts
+++ learningApp/src/app/app.module.ts
@@ -2,6 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ResourceComponent } from './resource/resource.component';
@@ -24,7 +47,28 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    MatSliderModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    DragDropModule,
+    MatSidenavModule,
+    MatTabsModule,
+    MatListModule,
+    MatCardModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- learningApp/src/app/forum/forum.component.ts
+++ learningApp/src/app/forum/forum.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+
+export interface ArticleListConfig {
+  type: string;
+
+  filters: {
+    tag?: string,
+    author?: string,
+    favorited?: string,
+    limit?: number,
+    offset?: number
+  };
+}
+
 
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
   styleUrls: ['./forum.component.css']
 })
+
 export class ForumComponent implements OnInit {
 
   constructor() { }
 
+  isAuthenticated: boolean;
+  listConfig: ArticleListConfig = {
+    type: 'all',
+    filters: {}
+  };
+  tags: Array<string> = [];
+  tagsLoaded = false;
+  content: string;
+
   ngOnInit(): void {
+  	this.isAuthenticated = true;
+  	this.content = 'Discuss';
+  }
+
+  getPosts(index: number){
+  	if(index === 1){
+  		this.content = 'Discuss';
+  	}
+  	else{
+  		this.content = 'My posts';
+  	}
+  	return this.content;
+  }
+
+  setListTo(type: string = '', filters: Object = {}) {
+    // If feed is requested but user is not authenticated, redirect to login
+    if (type === 'feed' && !this.isAuthenticated) {
+      return;
+    }
+
+    // Otherwise, set the list object
+    this.listConfig = {type: type, filters: filters};
   }
 
 }
+
+
