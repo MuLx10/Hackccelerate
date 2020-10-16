6c829fa1fb831c0f49bfb6cf1760f3c352c9c18f
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2020-10-16 23:29:37 +0530

    post

--- learningApp/src/app/forum/forum.module.ts
+++ learningApp/src/app/forum/forum.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostComponent } from './post/post.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PostComponent],
   imports: [
     CommonModule,
   ],
