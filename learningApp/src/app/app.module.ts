c3cbfb07d8acc6f3e8f05ff3d2016c36cc2526dc
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2020-10-16 21:13:27 +0530

    NavBar

--- learningApp/src/app/app.module.ts
+++ learningApp/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResourceComponent } from './resource/resource.component';
+import { HomeComponent } from './home/home.component';
+import { ForumComponent } from './forum/forum.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'forum', component: ForumComponent },
+  { path: 'resource', component: ResourceComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResourceComponent,
+    HomeComponent,
+    ForumComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
