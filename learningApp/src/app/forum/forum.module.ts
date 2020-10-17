ff3455f3015c89f7d2e12e1a7b2867b449238d07
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2020-10-17 20:57:12 +0530

    Compose Component

--- learningApp/src/app/forum/forum.module.ts
+++ learningApp/src/app/forum/forum.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post/post.component';
 import { ArticleComponent } from './article/article.component';
+import { ComposeComponent } from './compose/compose.component';
 
 
 @NgModule({
-  declarations: [PostComponent, ArticleComponent],
+  declarations: [PostComponent, ArticleComponent, ComposeComponent],
   imports: [
     CommonModule
   ],
